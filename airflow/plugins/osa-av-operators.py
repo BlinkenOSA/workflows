6d70644b714e02c22c7bf2d7b8a485e858e6f442
Dani Janos <dani.janos@gmail.com>
2018-04-17 15:17:09 +0200

    Add CreateDirectoryStructure class operator skeleton to osa-av-operators.py and add to it's operators array [issue #6]

--- airflow/plugins/osa-av-operators.py
+++ airflow/plugins/osa-av-operators.py
@@ -8,6 +8,20 @@ from airflow.operators.sensors import BaseSensorOperator
 
 log = logging.getLogger(__name__)
 
+
+class CreateDirectoryStructure(BaseOperator):
+
+	@apply_defaults
+	def __init__(self, param, *args, **kwargs):
+		self.operator_param = param
+		super(CreateDirectoryStructure, self).__init__(*args, **kwargs)
+
+	def execute(self, context):
+		log.info("CreateDirectoryStructure class execute method invoked")
+		log.info("parameter was: %s", self.operator_param)
+		task_instance = context["task_instance"]
+
+
 class OSA_AV_Plugins(AirflowPlugin):
 	name = "OSA AV Digitization workflow plugin"
-	operators = []
+	operators = [CreateDirectoryStructure]
