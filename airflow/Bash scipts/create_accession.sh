09d47aa82e658d344b425cc77850ea3c2b6523c9
Dani Janos <dani.janos@gmail.com>
2018-06-12 17:46:11 +0200

    Creating the basic stucture of the script

--- airflow/Bash scipts/create_accession.sh
+++ airflow/Bash scipts/create_accession.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+#   ----------------------------------------------------------------
+#   Function for transcoding master files
+#       Accepts 1 argument:
+#           string containing the path of the videos
+#   ----------------------------------------------------------------
+function do_transcoding_to_mp4()
+{
+    local video_directory_path=$1
+    echo "$video_directory_path in $program_name"   
+}
+
+program_name=$(basename $0)
+if [ "$#" -ne 1 ]
+then
+    echo "Illegal number of parameters"
+else
+    do_transcoding_to_mp4 $1
+fi
