268d17f7441b7a733fb2d9a31647081576b3250a
Dani Janos <dani.janos@gmail.com>
2018-06-14 11:25:21 +0200

    add output_file_name variable to do_transcoding_to_mp4 function

--- airflow/Bash scipts/create_accession.sh
+++ airflow/Bash scipts/create_accession.sh
@@ -8,14 +8,15 @@
 function do_transcoding_to_mp4()
 {
     local video_directory_path=$1
-    local file_extension="mp4"
+    local file_extension=".mp4"
     for video_files in $video_directory_path/*
     do
         # perhaps we should copy the file into a local directory first
         # first extract the filename and the extension from the path that given && then after extract only the name of it
         local video_file_name=$(basename $video_files) && video_file_name="${video_file_name%.*}"
-        echo $video_file_name
-    done        
+        local output_file_name=$video_file_name$file_extension
+        echo $output_file_name
+    done
 }
 
 program_name=$(basename $0)
