df265b6f6cc80f0ab687cd9ca47f5df3262f1807
JoshBone <jozsef.bone@gmail.com>
2018-08-16 14:21:57 +0200

    The main concept for recursive retriggering Airflow DAG.

--- airflow/dags/osa-av-workflow.py
+++ airflow/dags/osa-av-workflow.py
@@ -1,4 +1,6 @@
 from datetime import timedelta, datetime
+
+from airflow.operators.dagrun_operator import TriggerDagRunOperator
 from airflow.operators.python_operator import PythonOperator
 
 from airflow import DAG
@@ -19,7 +21,13 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
-dag = DAG('osa-av-workflow',
+
+def retrigger(context, dag_run_obj):
+    return dag_run_obj
+
+
+dag_id = 'osa-av-workflow'
+dag = DAG(dag_id=dag_id,
           description='Main DAG for the preservation workflow',
           default_args=default_args,
           schedule_interval=None,
@@ -29,7 +37,9 @@ task_01 = PythonOperator(task_id='collect_files', python_callable=collect_files,
 task_02 = PythonOperator(task_id='create_directories', python_callable=create_directories, dag=dag)
 task_03 = PythonOperator(task_id='copy_master_files', python_callable=copy_master_files, dag=dag)
 task_04 = PythonOperator(task_id='create_checksums', python_callable=create_checksums, dag=dag)
+task_05 = TriggerDagRunOperator(task_id='restart_dag', trigger_dag_id=dag_id, python_callable=retrigger, dag=dag)
 
 task_01.set_downstream(task_02)
 task_02.set_downstream(task_03)
 task_03.set_downstream(task_04)
+task_04.set_downstream(task_05)
