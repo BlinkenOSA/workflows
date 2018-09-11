09a85e9cec7c70d47add919bca69fa4b4a3171a3
Dani Janos <dani.janos@gmail.com>
2018-09-11 17:04:18 +0200

    add send_email task to the workflow

--- airflow/dags/osa-av-workflow.py
+++ airflow/dags/osa-av-workflow.py
@@ -13,6 +13,7 @@ from python_tasks.copy_master_files import copy_master_files
 from python_tasks.create_checksums import create_checksums
 from python_tasks.encode_masters import encode_masters
 from python_tasks.create_video_info import create_video_info
+from python_tasks.send_email import notify_email
 
 from python_tasks.config import INPUT_DIR, MASTER_FILE_EXTENSION, ACCESS_FILE_EXTENSION
 
@@ -38,36 +39,88 @@ def retrigger_dag(context, dag_run_obj):
 
 
 # DAG
-dag_id = 'osa-av-workflow'
-dag = DAG(dag_id=dag_id,
+dag = DAG(dag_id='osa-av-workflow',
           description='Main DAG for the preservation workflow',
           default_args=default_args,
           schedule_interval=None,
           catchup=False)
 
 # Tasks
-collect_files = PythonOperator(task_id='collect_files', python_callable=collect_files, dag=dag)
-create_directories = PythonOperator(task_id='create_directories', python_callable=create_directories, dag=dag)
-copy_master_files = PythonOperator(task_id='copy_master_files', python_callable=copy_master_files, dag=dag)
-create_master_checksums = PythonOperator(task_id='create_master_checksums', python_callable=create_checksums, dag=dag,
-                                         op_kwargs={'directory': 'Preservation',
-                                                    'file_extension': MASTER_FILE_EXTENSION})
-create_master_info = PythonOperator(task_id='create_master_info', python_callable=create_video_info, dag=dag,
-                                    op_kwargs={'directory': 'Preservation',
-                                               'file_extension': MASTER_FILE_EXTENSION})
-encode_masters = BranchPythonOperator(task_id='encode_masters', python_callable=encode_masters, dag=dag,
-                                      op_kwargs={'on_success': 'create_access_checksums',
-                                                 'on_error': 'break_dag'})
-create_access_checksums = PythonOperator(task_id='create_access_checksums', python_callable=create_checksums, dag=dag,
-                                         op_kwargs={'directory': 'Access',
-                                                    'file_extension': ACCESS_FILE_EXTENSION})
-create_access_info = PythonOperator(task_id='create_access_info', python_callable=create_video_info, dag=dag,
-                                    op_kwargs={'directory': 'Access',
-                                               'file_extension': ACCESS_FILE_EXTENSION})
+collect_files = PythonOperator(
+    task_id='collect_files',
+    python_callable=collect_files,
+    dag=dag)
 
-break_dag = DummyOperator(task_id='break_dag', dag=dag)
-retrigger_dag = TriggerDagRunOperator(task_id='restart_dag', trigger_dag_id=dag_id, python_callable=retrigger_dag,
-                                      dag=dag, trigger_rule='one_success')
+create_directories = PythonOperator(
+    task_id='create_directories',
+    python_callable=create_directories,
+    dag=dag)
+
+copy_master_files = PythonOperator(
+    task_id='copy_master_files',
+    python_callable=copy_master_files,
+    dag=dag)
+
+create_master_checksums = PythonOperator(
+    task_id='create_master_checksums',
+    python_callable=create_checksums,
+    dag=dag,
+    op_kwargs={
+        'directory': 'Preservation',
+        'file_extension': MASTER_FILE_EXTENSION
+    })
+
+create_master_info = PythonOperator(
+    task_id='create_master_info',
+    python_callable=create_video_info,
+    dag=dag,
+    op_kwargs={
+        'directory': 'Preservation',
+        'file_extension': MASTER_FILE_EXTENSION
+    })
+
+encode_masters = BranchPythonOperator(
+    task_id='encode_masters',
+    python_callable=encode_masters,
+    dag=dag,
+    op_kwargs={
+        'on_success': 'create_access_checksums',
+        'on_error': 'break_dag'
+    })
+
+create_access_checksums = PythonOperator(
+    task_id='create_access_checksums',
+    python_callable=create_checksums,
+    dag=dag,
+    op_kwargs={
+        'directory': 'Access',
+        'file_extension': ACCESS_FILE_EXTENSION
+    })
+
+create_access_info = PythonOperator(
+    task_id='create_access_info',
+    python_callable=create_video_info,
+    dag=dag,
+    op_kwargs={
+        'directory': 'Access',
+        'file_extension': ACCESS_FILE_EXTENSION
+    })
+
+break_dag = DummyOperator(
+    task_id='break_dag',
+    dag=dag)
+
+retrigger_dag = TriggerDagRunOperator(
+    task_id='restart_dag',
+    trigger_dag_id=dag_id,
+    python_callable=retrigger_dag,
+    dag=dag,
+    trigger_rule='one_success')
+
+send_info_mail = PythonOperator(
+    task_id='send_info_mail',
+    python_callable=notify_email,
+    dag=dag)
 
 # Flow
 collect_files.set_downstream(create_directories)
@@ -77,10 +130,12 @@ create_master_checksums.set_downstream(create_master_info)
 create_master_info.set_downstream(encode_masters)
 encode_masters.set_downstream(create_access_checksums)
 create_access_checksums.set_downstream(create_access_info)
-create_access_info.set_downstream(retrigger_dag)
+create_access_info.set_downstream(send_info_mail)
+send_info_mail.set_downstream(retrigger_dag)
 
 # Error branch
 encode_masters.set_downstream(break_dag)
-break_dag.set_downstream(retrigger_dag)
+break_dag.set_downstream(send_info_mail)
+send_info_mail.set_downstream(retrigger_dag)
 
 
