4923b8bcc1264db6a0b13f2c34d4a73aa5555d0a
JoshBone <jozsef.bone@gmail.com>
2018-10-19 15:23:14 +0200

    Remove unnecessary files...

--- set_environment_variables.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-export AIRFLOW__CORE__AIRFLOW_HOME="/path/to/our/airflow/stuff"
-export AIRFLOW__CORE__DAGS_FOLDER="${AIRFLOW__CORE__AIRFLOW_HOME}/dags"
-export AIRFLOW__CORE__SQL_ALCHEMY_CONN="mysql://airflow:airflow@localhost:3306/airflow"
-export AIRFLOW__CORE__LOAD_EXAMPLES="False"
-export AIRFLOW__CORE__FERNET_KEY="some_secret_key"
-export AIRFLOW__WEBSERVER__AUTH_BACKEND='airflow.contrib.auth.backends.password_auth'
-export AIRFLOW__WEBSERVER__AUTHENTICATE='True'
