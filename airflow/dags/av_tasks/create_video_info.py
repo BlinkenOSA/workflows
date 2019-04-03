ff822723bc90753c6bec1d40c9e22187dfa566f4
Dani János <dani.janos@gmail.com>
2019-04-03 14:27:25 +0000

    Changes form the server

--- airflow/dags/av_tasks/create_video_info.py
+++ airflow/dags/av_tasks/create_video_info.py
@@ -33,6 +33,7 @@ def create_video_info(directory='Preservation', file_extension='mpg'):
         volumes = {}
         volumes[os.path.join(WORKING_DIR, barcode)] = {'bind': docker_dir, 'mode': 'rw'}
 
+        log.info(WORKING_DIR)
         log.info("Starting ffmpeg docker, mapping %s as %s" % (os.path.join(WORKING_DIR, barcode), docker_dir))
 
         input_dir = os.path.join(docker_dir, 'Content', directory)
